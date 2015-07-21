19e248f22d236cdd1562c18d63f6d950eaa5d9b7
kazukgw <kazukgw@gmail.com>
2015-07-21 11:13:50 +0900

    PackNullとPackZeroに分割

--- timepack.go
+++ timepack.go
@@ -7,13 +7,21 @@ import (
 	"time"
 )
 
-var Format = "2006-01-02 15:04:05"
+var DefaultLayout = "2006-01-02 15:04:05"
 
-type Pack struct {
+type PackNull struct {
+	Layout string
 	time.Time
 }
 
-func (p *Pack) Scan(value interface{}) error {
+func NewNull(t time.Time) PackNull {
+	return PackNull{
+		Layout: DefaultLayout,
+		Time:   t,
+	}
+}
+
+func (p *PackNull) Scan(value interface{}) error {
 	if value == nil {
 		p.Time = time.Time{}
 		return nil
@@ -22,15 +30,22 @@ func (p *Pack) Scan(value interface{}) error {
 	return nil
 }
 
-func (p Pack) Value() (driver.Value, error) {
+func (p PackNull) Value() (driver.Value, error) {
 	if p.IsZero() {
 		return nil, nil
 	}
 	return p.Time, nil
 }
 
-func (p *Pack) UnmarshalJSON(data []byte) error {
-	newt, err := time.Parse(Format, strings.Replace(string(data), `"`, "", -1))
+func (p *PackNull) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		p.Time = time.Time{}
+		return nil
+	}
+	if p.Layout == "" {
+		p.Layout = DefaultLayout
+	}
+	newt, err := time.Parse(p.Layout, strings.Replace(string(data), `"`, "", -1))
 	if err != nil {
 		p.Time = time.Time{}
 		return err
@@ -39,21 +54,97 @@ func (p *Pack) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-func (p *Pack) UnmarshalText(text []byte) error {
+func (p *PackNull) UnmarshalText(text []byte) error {
 	return p.UnmarshalJSON(text)
 }
 
-func (p Pack) MarshalJSON() ([]byte, error) {
+func (p PackNull) MarshalJSON() ([]byte, error) {
 	if p.IsZero() {
 		return []byte("null"), nil
 	}
-	str := p.Time.Format(Format)
+	if p.Layout == "" {
+		p.Layout = DefaultLayout
+	}
+	str := p.Time.Format(p.Layout)
 	return json.Marshal(str)
 }
 
-func (p Pack) MarshalText() ([]byte, error) {
+func (p PackNull) MarshalText() ([]byte, error) {
 	if p.IsZero() {
 		return []byte{}, nil
 	}
-	return []byte(p.Time.Format(Format)), nil
+	if p.Layout == "" {
+		p.Layout = DefaultLayout
+	}
+	return []byte(p.Time.Format(p.Layout)), nil
+}
+
+type PackZero struct {
+	Layout string
+	time.Time
+}
+
+func NewZero(t time.Time) PackZero {
+	return PackZero{
+		Layout: DefaultLayout,
+		Time:   t,
+	}
+}
+
+func (p *PackZero) Scan(value interface{}) error {
+	if value == nil {
+		p.Time = time.Time{}
+		return nil
+	}
+	p.Time = value.(time.Time)
+	return nil
+}
+
+func (p PackZero) Value() (driver.Value, error) {
+	if p.IsZero() {
+		return time.Time{}, nil
+	}
+	return p.Time, nil
+}
+
+func (p *PackZero) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		p.Time = time.Time{}
+		return nil
+	}
+	if p.Layout == "" {
+		p.Layout = DefaultLayout
+	}
+	newt, err := time.Parse(p.Layout, strings.Replace(string(data), `"`, "", -1))
+	if err != nil {
+		p.Time = time.Time{}
+		return err
+	}
+	p.Time = newt
+	return nil
+}
+
+func (p *PackZero) UnmarshalText(text []byte) error {
+	return p.UnmarshalJSON(text)
+}
+
+func (p PackZero) MarshalJSON() ([]byte, error) {
+	if p.IsZero() {
+		return []byte(""), nil
+	}
+	if p.Layout == "" {
+		p.Layout = DefaultLayout
+	}
+	str := p.Time.Format(p.Layout)
+	return json.Marshal(str)
+}
+
+func (p PackZero) MarshalText() ([]byte, error) {
+	if p.IsZero() {
+		return []byte{}, nil
+	}
+	if p.Layout == "" {
+		p.Layout = DefaultLayout
+	}
+	return []byte(p.Time.Format(p.Layout)), nil
 }
